99dc8d5fa9cff1f9b83c8074296a204635bce7ed
Jay Newstrom <jaynewstrom@gmail.com>
2021-09-15 16:57:58 +0000

    OKTA-428340 Add the ability to initialize with a custom OkHttpClient.
    OKTA-428340
    <<<Jenkins Check-In of Tested SHA: 9631609446fd4b4dcc9cee5f0fd0c4dea289eba4 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: okta-commons-java
    Files changed count: 2
    PR Link: "https://github.com/okta/okta-commons-java/pull/60"

--- http/okhttp/src/main/java/com/okta/commons/http/okhttp/OkHttpRequestExecutorFactory.java
+++ http/okhttp/src/main/java/com/okta/commons/http/okhttp/OkHttpRequestExecutorFactory.java
@@ -20,6 +20,7 @@ import com.okta.commons.http.RequestExecutor;
 import com.okta.commons.http.RequestExecutorFactory;
 import com.okta.commons.http.RetryRequestExecutor;
 import com.okta.commons.http.config.HttpClientConfiguration;
+import okhttp3.OkHttpClient;
 
 /**
  * @since 1.2.0
@@ -27,8 +28,24 @@ import com.okta.commons.http.config.HttpClientConfiguration;
 @AutoService(RequestExecutorFactory.class)
 public class OkHttpRequestExecutorFactory implements RequestExecutorFactory {
 
+    private final OkHttpClient client;
+
+    public OkHttpRequestExecutorFactory() {
+        this(null);
+    }
+
+    public OkHttpRequestExecutorFactory(OkHttpClient client) {
+        this.client = client;
+    }
+
     @Override
     public RequestExecutor create(HttpClientConfiguration clientConfiguration) {
-        return new RetryRequestExecutor(clientConfiguration, new OkHttpRequestExecutor(clientConfiguration));
+        OkHttpRequestExecutor executor;
+        if (client != null) {
+            executor = new OkHttpRequestExecutor(clientConfiguration, client);
+        } else {
+            executor = new OkHttpRequestExecutor(clientConfiguration);
+        }
+        return new RetryRequestExecutor(clientConfiguration, executor);
     }
 }
